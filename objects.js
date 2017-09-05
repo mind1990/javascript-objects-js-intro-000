340bce20b8f91229796783d1f104687a043047e0
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-05 06:13:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { LP: 'Some where I belong'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign({}, playlist, { [artistName]: songTitle })
+  Object.assign(playlist, { [artistName]: songTitle })
   return playlist
 }
