34af9f34e38b6b6c589104b0928ced644eaf71f6
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-05 06:16:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, { [artistName]: songTitle })
 
 }
+
+
+function removeFromPlaylist(playlist, artistName) {
+  delete.playist.artistName
+}
