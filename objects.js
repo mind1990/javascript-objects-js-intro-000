db092721dfa19a9118c2004c859ff73e471b1c48
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-05 07:29:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { LinkinPark: "Some where I belong"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist = Object.assign({}, playlist, { [artistName]: songTitle })
+  playlist.artistName = 'songTitle'
   return playlist
 }
 
